887705a438e6f326f666491b280b3bae031e70c4
Jack <Jack@Jack-PC>
2015-12-01 19:35:52 -0600

    DeleteCommand

--- persistentDataStoreStudent5348/src/utd/persistentDataStore/datastoreServer/commands/DeleteCommand.java
+++ persistentDataStoreStudent5348/src/utd/persistentDataStore/datastoreServer/commands/DeleteCommand.java
@@ -2,13 +2,24 @@ package utd.persistentDataStore.datastoreServer.commands;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
+import utd.persistentDataStore.utils.FileUtil;
 import utd.persistentDataStore.utils.ServerException;
-
+import utd.persistentDataStore.utils.StreamUtil;
 public class DeleteCommand extends ServerCommand{
-
+	private static Logger logger = Logger.getLogger(DeleteCommand.class);
 	@Override
 	public void run() throws IOException, ServerException {
 		// TODO Auto-generated method stub
+		String name = StreamUtil.readLine(inputStream);
+		if(name==null) throw new ServerException("Cannot get name");
+		logger.debug("name: " + name);
+		boolean flag = false;
+		flag =FileUtil.deleteData(name);
+		if(flag==true)sendOK();
+		else sendError("Can not find the file");
+		
 		
 	}
 
